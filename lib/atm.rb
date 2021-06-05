aefadb9629a7b7f6b79ad80ad501d8a9003358a7
DavDan1 <david-danielyan@hotmail.com>
2021-06-05 18:31:56 +0200

    adds sad path when user has insuificient funds

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,21 +1,28 @@
 class Atm
-    attr_accessor :funds
+  attr_accessor :funds
 
-    def initialize
-        @funds=1000
+  def initialize
+    @funds = 1000
+  end
+
+  def withdraw(amount, account)
+    if insufficient_funds_in_account?(amount, account)
+      nil
+
+    else
+      perform_transaction(amount, account)
     end
+  end
 
-    def withdraw(amount, account)
-        
-        case 
-        when  amount > account.balance
-            return
-            
-        else
-            @funds -= amount
-            account.balance = account.balance - amount
-            { status: true, message: 'success', date: Date.today , amount: amount}
-        end
-    end
+  private
 
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transaction(amount, account)
+    @funds -= amount
+    account.balance = account.balance - amount
+    { status: true, message: 'success', date: Date.today, amount: amount }
+  end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -6,24 +6,28 @@ describe Atm do
   before do
     allow(account).to receive(:balance).and_return(100)
     allow(account).to receive(:balance=)
-end
-    
-    it 'is expected to be an instance of Atm' do
-        expect(subject.class).to eq Atm
-    end
-
-    it 'has 1000$ on initialize' do
-        expect(subject.funds).to eq 1000
-    end
-    
-    it 'funds are reduced at withdraw' do
-        subject.withdraw(50)
-        expect(subject.funds).to eq 950
-    end
-
-    it 'allow withdraw if the account has enough balance.' do
-      expected_output = { status: true, message: 'success', date: Date.today, amount: 45}
-      expect(subject.withdraw(45,account)).to eq expected_output
   end
 
+  it 'is expected to be an instance of Atm' do
+    expect(subject.class).to eq Atm
+  end
+
+  it 'has 1000$ on initialize' do
+    expect(subject.funds).to eq 1000
+  end
+
+  it 'funds are reduced at withdraw' do
+    subject.withdraw(50)
+    expect(subject.funds).to eq 950
+  end
+
+  it 'allow withdraw if the account has enough balance.' do
+    expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+    expect(subject.withdraw(45, account)).to eq expected_output
+  end
+
+  it 'rejects withdraw if account has insufficient funds' do
+    expected_output = { status: false, message: 'insufficient funds', date: Date.today }
+    expect(subject.withdraw(105, account)).to eq expected_output
+  end
 end
